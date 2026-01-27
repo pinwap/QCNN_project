659a6a1d1df5e8db69ec1b046484da83a894936a
pinwap <156493711+pinwap@users.noreply.github.com>
2026-01-27 15:39:04 +0700

    สร้าง qcnn_shared ให้ feature map ที่ทั้ง 2 โมเดลใช้ร่วมกัน

---
+++ qcnn_shared/__init__.py
@@ -0,0 +1,15 @@
+from .feature_maps import (
+    FeatureMapBuilder,
+    AngleEncodingMap,
+    PGEEncodingMap,
+    ZZFeatureMapBuilder,
+    resolve_feature_map,
+)
+
+__all__ = [
+    "FeatureMapBuilder",
+    "AngleEncodingMap",
+    "PGEEncodingMap",
+    "ZZFeatureMapBuilder",
+    "resolve_feature_map",
+]
